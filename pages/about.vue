874a268f0d18f75173dae0b0798fe6e848556762
antoine <bycdmcyigbas@gmail.com>
2017-11-13 11:06:35 -0800

    Using Nuxt to generate a SPA with Vue

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <h1>This is page About!</h1>
+  <nuxt-link to="/" class="button--green red">Let's go home</nuxt-link>
+</div>
+</template>
+
+<style>
+.red{
+  color: tomato;
+}  
+</style>
