144bf0275646ed57447621f3e2d7b54672371d52
antoine <bycdmcyigbas@gmail.com>
2018-01-06 12:01:58 -0800

    responsive nav bar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,10 +2,10 @@
 <div class='Navbar'>
   <div class='container'>
     <div class='row align-items-center'>
-      <div class='offset-sm-1 col-md-2 col-sm-1'>
+      <div class='offset-sm-1 col-sm-2 col-3'>
         <img src='/img/logo.svg' class='img-fluid'>
       </div>
-      <div class='nav offset-sm-4 col-sm-4'>
+      <div class='nav offset-sm-4 col-sm-4 col-9'>
         <a class='header-link active' href='#about'>ABOUT</a>
         <a class='header-link' href='#projects'>PROJECTS</a>
         <a class='header-link' href='#contact'>CONTACT</a>
@@ -32,10 +32,14 @@ export default {
 }
 
 .header-link{
-  padding: 8px 16px;
   color: $text-primary;
-  font-size: 16px;
   text-align: center;
+  padding: 4px 4px;
+  font-size: 14px;
+  @media (min-width: 576px){
+    padding: 8px 16px;
+    font-size: 16px;
+  } 
 }
 
 .header-link.active{
