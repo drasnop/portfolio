efb814bdedac6eec4f60c656d929beed70fc40db
antoine <bycdmcyigbas@gmail.com>
2018-04-28 10:46:06 -0700

    don't need about test page anymore

--- pages/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class='containter'>
-  <h1>This is page About!</h1>
-  <nuxt-link to="/" class="button">Let's go home</nuxt-link>
-</div>
-</template>
-
-<style lang='scss'>
-</style>
