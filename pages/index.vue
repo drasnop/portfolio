047ac97d4b734f2dc513c28a862d535af4fa4594
antoine <bycdmcyigbas@gmail.com>
2018-04-28 16:57:40 -0700

    large images + light gray on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
 
       <div class='offset-sm-2 col-sm-4 align-self-end pos-relative'>
         <img src='/img/antoine-small.jpg' class='img-fluid img-sizing'>
-        <img src='/img/antoine-small.jpg' class='img-fluid img-raised img-floating' style='left:15px; bottom:-80%'>
+        <img src='/img/antoine-small.jpg' class='img-fluid img-raised img-floating antoine-picture' style='left:15px'>
       </div>
     </div>
   </div>
@@ -27,13 +27,13 @@
 <div class='section blue-section'>
   <div class='container'>
     <div class='row align-items-center extra-padding-top'>
-      <div class='project-text offset-sm-3 col-sm-4 order-sm-12'>
+      <div class='project-text offset-sm-1 col-sm-5 order-sm-12'>
         <h2>Xodo</h2>
         <p class='project-short-description'>Xodo is a cross-platform app for reading, annotating, and collaborating on documents. It has millions of users and a 4.7 out of 5 rating in the Google PlayStore. I am leading the design of a brand new version of the app (in beta now), which will significantly expand its collaboration capabilities. I tackle a wide range of challenges, from product direction to user flows, wireframes, and visual identity.</p>
         <div class="link-button align-self-center" text="Don't click this button" @click='xodoAlert'>Don't click this button</div>
       </div>
-      <div class='offset-sm-1 col-sm-3 order-sm-1 pos-relative'>
-        <img src='/img/xodo-small.png' class='img-fluid img-raised'>
+      <div class='offset-sm-1 col-sm-4 order-sm-1 pos-relative'>
+        <img src='/img/xodo-small.png' class='img-fluid img-raised xodo-img'>
       </div>
     </div>
   </div>
@@ -47,7 +47,7 @@
         <p class='project-short-description'>The literature review is a key component of academic research, that allows researchers to build upon each other’s work. PaperQuest is a visualization tool that supports efficient decision making, by only displaying the information useful at a given step of the exploration process.</p>
         <link-button text="What is your quest?" to='/paperquest'></link-button>
       </div>
-      <div class='offset-sm-2 col-sm-4 pos-relative'>
+      <div class='offset-sm-1 col-sm-5 pos-relative'>
         <img src='/img/paperquest-small.png' class='img-fluid img-raised'/>
       </div>
     </div>
@@ -57,13 +57,13 @@
 <div class='section blue-section'>
   <div class='container'>
     <div class='row align-items-center'>
-      <div class='project-text offset-sm-2 col-sm-4 order-sm-12'>
-        <h2 style='font-size:1.9em'>Anchored Customization</h2>
+      <div class='project-text offset-sm-1 col-sm-4 order-sm-12'>
+        <h2>Anchored Customization</h2>
         <p class='project-short-description'>Settings panels are the most common customization mechanism in today's
-software. Yet it is often difficult to find the setting you are looking for. I solve this problem by introducing the customization layer, which overlays settings directly on top of the application interface. </p>
+        software. Yet it is often difficult to find the setting you are looking for. I solve this problem by introducing the customization layer, which overlays settings directly on top of the application interface. </p>
         <link-button text="Customize all the things" to='/customization'></link-button>
       </div>
-      <div class='offset-sm-1 col-sm-4 order-sm-1 pos-relative'>
+      <div class='offset-sm-1 col-sm-5 order-sm-1 pos-relative'>
         <img src='/img/customization-small.png' class='img-fluid img-raised'>
       </div>
     </div>
@@ -78,7 +78,7 @@ software. Yet it is often difficult to find the setting you are looking for. I s
         <p class='project-short-description'>The concept of ephemeral adaptation was introduced to reduce visual search time in GUI menus, while preserving spatial consistency and minimizing distraction. We extend this concept to the visual search of app icons on smartphones in order to speed up launching apps from a homescreen.</p>
         <link-button text="Shake it up, baby" to='/twistandpulse'></link-button>
       </div>
-      <div class='offset-sm-2 col-sm-4 pos-relative'>
+      <div class='offset-sm-1 col-sm-5 pos-relative'>
         <img src='/img/twistAndPulse.png' class='img-fluid img-raised'>
       </div>
     </div>
@@ -124,7 +124,9 @@ export default {
 }
 
 .blue-section{
-  background-color: $section-bg-color;
+  background-color: hsla(199, 30%, 98%, 1);
+  border-top: 1px solid #eee;
+  border-bottom: 1px solid #eee;
 }
 
 .row.extra-padding-top{
@@ -148,4 +150,20 @@ export default {
   font-style: 16px;
   line-height: 1.6em;
 }
+
+.antoine-picture{
+  bottom:-70%;
+  @media (max-width: $break-small){
+    bottom: -80%;
+  }
+}
+
+img.xodo-img{
+  @media (min-width: $break-small){
+    max-width: 75%;
+    left:7%;
+    position: relative;
+  }
+}
+
 </style>
